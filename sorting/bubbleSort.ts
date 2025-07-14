32edac7abf201984433fe6cf31d6d96ec37bcacc
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-07-14 18:07:43 +0530

    fixed a bug in bubble sort

--- sorting/bubbleSort.ts
+++ sorting/bubbleSort.ts
@@ -2,16 +2,20 @@ function bubble(array: number[]) {
   let index = array.length;
 
   for (let i = 0; i < index - 1; i++) {
-    for (let j = 1; j < index; j++) {
+    let flag = 0;
+    for (let j = 0; j < index - 1 - i; j++) {
       let temp: number;
+
       if (array[j] > array[j + 1]) {
         temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
+        flag = 1;
       }
     }
+    if (flag == 0) return;
   }
   console.log(array);
 }
 
-bubble([20, 50, 40, 30, 20]);
+bubble([1000, 50, 40, 30, 20]);
